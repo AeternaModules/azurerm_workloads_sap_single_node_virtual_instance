85817c98af859621669bbf958948126b1ac37e6c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:24:33 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,69 @@
+resource "azurerm_workloads_sap_single_node_virtual_instance" "workloads_sap_single_node_virtual_instances" {
+  for_each = var.workloads_sap_single_node_virtual_instances
+
+  app_location                          = each.value.app_location
+  environment                           = each.value.environment
+  location                              = each.value.location
+  name                                  = each.value.name
+  resource_group_name                   = each.value.resource_group_name
+  sap_fqdn                              = each.value.sap_fqdn
+  sap_product                           = each.value.sap_product
+  managed_resource_group_name           = each.value.managed_resource_group_name
+  managed_resources_network_access_type = each.value.managed_resources_network_access_type
+  tags                                  = each.value.tags
+
+  single_server_configuration {
+    app_resource_group_name = each.value.single_server_configuration.app_resource_group_name
+    database_type           = each.value.single_server_configuration.database_type
+    dynamic "disk_volume_configuration" {
+      for_each = each.value.single_server_configuration.disk_volume_configuration != null ? [each.value.single_server_configuration.disk_volume_configuration] : []
+      content {
+        number_of_disks = disk_volume_configuration.value.number_of_disks
+        size_in_gb      = disk_volume_configuration.value.size_in_gb
+        sku_name        = disk_volume_configuration.value.sku_name
+        volume_name     = disk_volume_configuration.value.volume_name
+      }
+    }
+    secondary_ip_enabled = each.value.single_server_configuration.secondary_ip_enabled
+    subnet_id            = each.value.single_server_configuration.subnet_id
+    virtual_machine_configuration {
+      image {
+        offer     = each.value.single_server_configuration.virtual_machine_configuration.image.offer
+        publisher = each.value.single_server_configuration.virtual_machine_configuration.image.publisher
+        sku       = each.value.single_server_configuration.virtual_machine_configuration.image.sku
+        version   = each.value.single_server_configuration.virtual_machine_configuration.image.version
+      }
+      os_profile {
+        admin_username  = each.value.single_server_configuration.virtual_machine_configuration.os_profile.admin_username
+        ssh_private_key = each.value.single_server_configuration.virtual_machine_configuration.os_profile.ssh_private_key
+        ssh_public_key  = each.value.single_server_configuration.virtual_machine_configuration.os_profile.ssh_public_key
+      }
+      virtual_machine_size = each.value.single_server_configuration.virtual_machine_configuration.virtual_machine_size
+    }
+    dynamic "virtual_machine_resource_names" {
+      for_each = each.value.single_server_configuration.virtual_machine_resource_names != null ? [each.value.single_server_configuration.virtual_machine_resource_names] : []
+      content {
+        dynamic "data_disk" {
+          for_each = virtual_machine_resource_names.value.data_disk != null ? [virtual_machine_resource_names.value.data_disk] : []
+          content {
+            names       = data_disk.value.names
+            volume_name = data_disk.value.volume_name
+          }
+        }
+        host_name               = virtual_machine_resource_names.value.host_name
+        network_interface_names = virtual_machine_resource_names.value.network_interface_names
+        os_disk_name            = virtual_machine_resource_names.value.os_disk_name
+        virtual_machine_name    = virtual_machine_resource_names.value.virtual_machine_name
+      }
+    }
+  }
+
+  dynamic "identity" {
+    for_each = each.value.identity != null ? [each.value.identity] : []
+    content {
+      identity_ids = identity.value.identity_ids
+      type         = identity.value.type
+    }
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,103 @@
+variable "workloads_sap_single_node_virtual_instances" {
+  description = <<EOT
+Map of workloads_sap_single_node_virtual_instances, attributes below
+Required:
+    - app_location
+    - environment
+    - location
+    - name
+    - resource_group_name
+    - sap_fqdn
+    - sap_product
+    - single_server_configuration (block):
+        - app_resource_group_name (required)
+        - database_type (optional)
+        - disk_volume_configuration (optional, block):
+            - number_of_disks (required)
+            - size_in_gb (required)
+            - sku_name (required)
+            - volume_name (required)
+        - secondary_ip_enabled (optional)
+        - subnet_id (required)
+        - virtual_machine_configuration (required, block):
+            - image (required, block):
+                - offer (required)
+                - publisher (required)
+                - sku (required)
+                - version (required)
+            - os_profile (required, block):
+                - admin_username (required)
+                - ssh_private_key (required)
+                - ssh_public_key (required)
+            - virtual_machine_size (required)
+        - virtual_machine_resource_names (optional, block):
+            - data_disk (optional, block):
+                - names (required)
+                - volume_name (required)
+            - host_name (optional)
+            - network_interface_names (optional)
+            - os_disk_name (optional)
+            - virtual_machine_name (optional)
+Optional:
+    - managed_resource_group_name
+    - managed_resources_network_access_type
+    - tags
+    - identity (block):
+        - identity_ids (required)
+        - type (required)
+EOT
+
+  type = map(object({
+    app_location                          = string
+    environment                           = string
+    location                              = string
+    name                                  = string
+    resource_group_name                   = string
+    sap_fqdn                              = string
+    sap_product                           = string
+    managed_resource_group_name           = optional(string)
+    managed_resources_network_access_type = optional(string, "Public")
+    tags                                  = optional(map(string))
+    single_server_configuration = object({
+      app_resource_group_name = string
+      database_type           = optional(string)
+      disk_volume_configuration = optional(object({
+        number_of_disks = number
+        size_in_gb      = number
+        sku_name        = string
+        volume_name     = string
+      }))
+      secondary_ip_enabled = optional(bool, false)
+      subnet_id            = string
+      virtual_machine_configuration = object({
+        image = object({
+          offer     = string
+          publisher = string
+          sku       = string
+          version   = string
+        })
+        os_profile = object({
+          admin_username  = string
+          ssh_private_key = string
+          ssh_public_key  = string
+        })
+        virtual_machine_size = string
+      })
+      virtual_machine_resource_names = optional(object({
+        data_disk = optional(object({
+          names       = list(string)
+          volume_name = string
+        }))
+        host_name               = optional(string)
+        network_interface_names = optional(list(string))
+        os_disk_name            = optional(string)
+        virtual_machine_name    = optional(string)
+      }))
+    })
+    identity = optional(object({
+      identity_ids = set(string)
+      type         = string
+    }))
+  }))
+}
+
